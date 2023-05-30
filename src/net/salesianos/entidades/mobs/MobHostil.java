5d2051434bd9b23bc5dea3521dfc3f118aaa6eb6
CristopherGarcia2000 <usr3587@salesianos-lacuesta.net>
2023-05-30 14:02:57 +0100

    feat: Creada Ãla clase MobHostil que hereda de Mob e implementada la interfaz Combate

---
+++ src/net/salesianos/entidades/mobs/MobHostil.java
@@ -0,0 +1,23 @@
+package net.salesianos.entidades.mobs;
+
+import net.salesianos.entidades.Mob;
+import net.salesianos.interfaces.Combate;
+
+public abstract class MobHostil extends Mob implements Combate {
+    protected int fuerza;
+    public MobHostil(int salud,int fuerza) {
+        super(salud);
+        this.fuerza = fuerza;
+    }
+
+    @Override
+    public int atacar() {
+        return this.fuerza;
+    }
+
+    @Override
+    public void recibirAtaque(int ataque) {
+        this.salud = this.salud - ataque;
+        System.out.println("Ha recibido "+ataque+" puntos de daño, le quedan "+salud+" puntos de salud");
+    }
+}
