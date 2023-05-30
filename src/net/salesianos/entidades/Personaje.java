ca904a00175b52d425df9dd8c9ff2234e4b44ebc
CristopherGarcia2000 <usr3587@salesianos-lacuesta.net>
2023-05-30 12:29:28 +0100

    feat:Creada la clase Personaje con atributos y un constructor

---
+++ src/net/salesianos/entidades/Personaje.java
@@ -0,0 +1,13 @@
+package net.salesianos.entidades;
+
+public class Personaje {
+    private int salud;
+    private int defensa;
+    private int fuerza;
+
+    public Personaje(int defensa, int fuerza) {
+        this.salud = 12;
+        this.defensa = defensa;
+        this.fuerza = fuerza;
+    }
+}
