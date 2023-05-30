3a2542bb185e9239a8a6c2ea491b32baac35f8d5
CristopherGarcia2000 <usr3587@salesianos-lacuesta.net>
2023-05-30 12:37:32 +0100

    feat: Creada la clase abstracta Mob

---
+++ src/net/salesianos/entidades/Mob.java
@@ -0,0 +1,11 @@
+package net.salesianos.entidades;
+
+public abstract class Mob {
+    private int salud;
+
+    public Mob(int salud) {
+        this.salud = salud;
+    }
+
+    protected abstract void Moverse();
+}
